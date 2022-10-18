debe9f7434767c1e4a1af1508159742e1d7bf5f3
Mikhail Makurov/m1n1tea <113550126+m1n1tea@users.noreply.github.com>
2022-10-18 12:12:36 +0300

    Add files via upload

---
+++ 1559A.cpp
@@ -0,0 +1,24 @@
+﻿// 1559A.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+
+int main()
+{
+	int n, t;
+	int x, x1;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		x1 = -1;
+		std::cin >> n;
+		for (int j = 0; j < n; j++) {
+			std::cin >> x;
+			if (x1 == -1)
+				x1 = x;
+			x1 = x1 & x;
+		}
+		std::cout << x1 << " \n";
+	}
+
+	return 0;
+}
