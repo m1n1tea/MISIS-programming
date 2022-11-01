c752b8103c7bc6414f8ed55ecdd8407b1f605166
Mikhail Makurov/m1n1tea <113550126+m1n1tea@users.noreply.github.com>
2022-11-01 11:59:10 +0300

    Add files via upload

---
+++ makurov_m_p/1165B.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	std::vector<int> a(n+1);
+	a[0] = 0;
+	for (int i = 1; i <= n; i++) {
+		std::cin >> a[i];
+	}
+	std::sort(a.begin(),a.end());
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		if (a[i] > ans) {
+			ans++;
+		}
+	}
+	std::cout << ans;
+	
+	return 0;
+}
