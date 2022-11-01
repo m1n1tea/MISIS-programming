d5dbb5026bf8bd96183769fcdacbfd1d7c241811
Mikhail Makurov/m1n1tea <113550126+m1n1tea@users.noreply.github.com>
2022-11-01 11:26:41 +0300

    Add files via upload

---
+++ makurov_m_p/879B.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+	int64_t n, k;
+	std::cin >> n >> k;
+	std::vector<int64_t> a(n);
+	for (int64_t& x : a)
+		std::cin >> x;
+	int64_t cnt = 0;
+	int64_t ans=0;
+	for (int i = 0; i < n-1; i++) {
+		if (a[i] == n) {
+			ans = i;
+			break;
+		}
+		if (a[i+1] < a[ans]) {
+			if (cnt == 0)
+				ans = i;
+			cnt++;
+		}
+		else {
+			cnt = 0;
+			ans = i + 1;
+		}
+		if (cnt == k)
+			break;
+		if (i == n - 2)
+			ans = n-1;
+	}
+	std::cout << a[ans];
+
+
+	return 0;
+}
